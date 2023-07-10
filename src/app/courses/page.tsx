f7877e2f4445bf6bf634932f3a2ccba4136e5e26
noams24 <noams299@gmail.com>
2023-07-10 23:58:46 +0300

    commit

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -5,21 +5,21 @@ import { UserAccountNav } from '@/components/UserAccountNav'
 import { buttonVariants } from '@/components/ui/button'
 import Link from 'next/link'
 const Courses = async () => {
-  const session = await getAuthSession()
+  //const session = await getAuthSession()
   const testimonial1 = getListPage("sections/ComputerScience.md");
   const testimonial2 = getListPage("sections/Math.md");
   const testimonial3 = getListPage("sections/Other.md");
   return (
     <>
-    
+    {/* 
     {session?.user ? (
           <UserAccountNav user={session.user} />
         ) : (
           <Link href='/sign-in' className={buttonVariants()}>
             Sign In
           </Link>
-        )} 
-      {session ? <p>אתה מחובר</p> : <p>אתה לא מחובר</p>}
+        )}  
+      {session ? <p>אתה מחובר</p> : <p>אתה לא מחובר</p>} */}
       <Testimonials data={testimonial1} /> 
       <Testimonials data={testimonial2} /> 
       <Testimonials data={testimonial3} /> 
