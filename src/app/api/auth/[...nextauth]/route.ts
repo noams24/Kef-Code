78e20c15a5ee09f47f824023349641291a341ca5
noams24 <noams299@gmail.com>
2023-07-18 18:32:18 +0300

    auth error

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import { authOptions } from "@/lib/auth"
 import NextAuth from "next-auth"
 
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
+export default NextAuth(authOptions);
+//const handler = NextAuth(authOptions)
+//export { handler as GET, handler as POST }
