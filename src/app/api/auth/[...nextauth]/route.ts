f0fba34c573d53e6f01d39fd52824239db715216
Noam <80849999+noams24@users.noreply.github.com>
2023-07-18 18:37:34 +0300

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,6 @@ import { authOptions } from "@/lib/auth"
 import NextAuth from "next-auth"
 
 export default NextAuth(authOptions);
+
 //const handler = NextAuth(authOptions)
 //export { handler as GET, handler as POST }
