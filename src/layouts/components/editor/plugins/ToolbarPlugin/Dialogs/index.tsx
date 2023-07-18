8a7c8cb31c18828fb5347f1dc91afa2d0a6428e7
noams24 <noams299@gmail.com>
2023-07-18 13:19:58 +0300

    update homepage

--- src/layouts/components/editor/plugins/ToolbarPlugin/Dialogs/index.tsx
+++
@@ -1,6 +0,0 @@
-import GraphDialog from "./GraphDialog";
-import SketchDialog from "./SketchDialog";
-import ImageDialog from "./ImageDialog";
-import TableDialog from "./TableDialog";
-
-export { GraphDialog, SketchDialog, ImageDialog, TableDialog };
--- src/layouts/components/editor/types.ts
+++
@@ -1,2 +0,0 @@
-export type * from 'lexical'
-export type * from "@lexical/rich-text";
