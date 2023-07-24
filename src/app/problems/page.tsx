c5004eb462e68508ed4c778609670da10167c6cb
noams24 <noams299@gmail.com>
2023-07-24 11:45:43 +0300

    fixed digest bug

--- src/app/problems/page.tsx
+++ src/app/problems/page.tsx
@@ -1,14 +1,10 @@
-import ImageFallback from "@/helpers/ImageFallback";
 import MDXContent from "@/helpers/MDXContent";
 import { getListPage } from "@/lib/contentParser";
-import { markdownify } from "@/lib/utils/textConverter";
-import SeoMeta from "@/partials/SeoMeta";
 import { RegularPage } from "@/types";
 
 const singleProblem = () => {
   const data: RegularPage = getListPage("pages/pageproblemexample.md");
   const { frontmatter, content } = data;
-  //const { title, meta_title, description, image } = frontmatter;
 
   return (
     <>
