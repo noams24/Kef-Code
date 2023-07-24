fe2e3716bae0714f598ff6dd54828e6d718c3594
noams24 <noams299@gmail.com>
2023-07-24 12:18:45 +0300

    add webpack

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ const config = require("./src/config/config.json");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({ test: /\.anyextension$/, type: "asset/source" });
+    return config;
+  },
   reactStrictMode: true,
   basePath: config.base_path !== "/" ? config.base_path : "",
   trailingSlash: config.site.trailing_slash,
