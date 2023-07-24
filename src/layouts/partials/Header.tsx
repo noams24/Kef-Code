df9e16ad2574aa55119fb61fe47b56eedc88183e
noams24 <noams299@gmail.com>
2023-07-24 11:47:04 +0300

    auth

--- src/layouts/partials/Header.tsx
+++ src/layouts/partials/Header.tsx
@@ -4,8 +4,8 @@ import config from "@/config/config.json";
 import menu from "@/config/menu.json";
 import Link from "next/link";
 import React from "react";
-//import { authOptions } from '@/lib/auth'
-//import { getServerSession } from 'next-auth'
+import { authOptions } from '@/lib/auth'
+import { getServerSession } from 'next-auth'
 //import { UserAccountNav } from '@/components/UserAccountNav'
 //  child navigation link interface
 export interface IChildNavigationLink {
@@ -22,7 +22,7 @@ export interface INavigationLink {
 }
 
 const Header = async () => {
-  //const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions)
   // distructuring the main menu from menu object
   const { main }: { main: INavigationLink[] } = menu;
   const { navigation_button, settings } = config;
