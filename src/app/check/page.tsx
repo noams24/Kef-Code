b1f9be8f9a7d91e03339273a07c6c78bebfc71b9
noams24 <noams299@gmail.com>
2023-07-25 15:36:41 +0300

    fixing bug

--- src/app/check/page.tsx
+++ src/app/check/page.tsx
@@ -8,23 +8,14 @@ import { UserAccountNav } from '@/components/UserAccountNav'
 import Link from "next/link";
 const singleProblem = async () => {
   //const data: RegularPage = getListPage("pages/pageproblemexample.md");
-  const data: RegularPage = await getListPage("pages/about.md");
+  const data: RegularPage = getListPage("pages/about.md");
   //const { frontmatter, content } = data;
-  const session = await getServerSession(authOptions);
+  //const session = await getServerSession(authOptions);
   return (
     <>
              
           
-          {session?.user ? (
-          <UserAccountNav user={session.user} />
-        ) : (
-            <Link
-            className="btn btn-outline-primary btn-sm hidden lg:inline-block"
-            href="/sign-in"
-          >
-            כניסה
-          </Link>
-        )}        
+           
             {/*<div className="text-right flex mt-4">
               <div className="content">
                 <MDXContent content={content} />
