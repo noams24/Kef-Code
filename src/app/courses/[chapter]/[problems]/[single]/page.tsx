23d2557e48223ef1f229dbd0dd6b1aa149002471
noams24 <noams299@gmail.com>
2023-07-29 12:04:38 +0300

    update

--- src/app/courses/[chapter]/[problems]/[single]/page.tsx
+++ src/app/courses/[chapter]/[problems]/[single]/page.tsx
@@ -1,11 +1,17 @@
-import MDXContent from "@/helpers/MDXContent";
+//import MDXContent from "@/helpers/MDXContent";
 import { getListPage } from "@/lib/contentParser";
 import { RegularPage } from "@/types";
 import { Editor } from "@/components/text-editor/editor";
-import { authOptions } from '@/lib/auth'
-import { getServerSession } from 'next-auth'
-import { getSinglePage } from "@/lib/contentParser";
+//import { authOptions } from '@/lib/auth'
+//import { getServerSession } from 'next-auth'
+//import { getSinglePage } from "@/lib/contentParser";
 import PageHeader from "@/partials/PageHeaderr";
+import Tab from "@/shortcodes/Tab";
+import Tabs from "@/shortcodes/Tabs";
+import Video from "@/shortcodes/Video";
+import Likes from "@/shortcodes/Likes";
+import Accordion from "@/shortcodes/Accordion";
+import ImageDisplay from "@/components/ImageDisplay"
 
 interface PageProps {
     params: {
@@ -19,12 +25,29 @@ const singleProblem = async ({ params }: PageProps) => {
     const data: RegularPage = getListPage("pages/pageproblemexample.md");
     const { frontmatter, content } = data;
     //const session = await getServerSession(authOptions);
+    const imageUrl = "https://i.ibb.co/Gdz4BTg/problem1.png";
     return (
         <>
-            <PageHeader title={params.single}/>
+        <PageHeader title={params.single}/>
+        <div className="flex justify-center">
+            
             <div className="text-right flex mt-4">
-                <div className="content">
-                    <MDXContent content={content} />
+            <div className="content">
+                    <Tabs>
+                    <Tab name="פתרונות">כאן יופיעו פתרונות של אנשים</Tab>
+                    <Tab name="פתרון רשמי"><Video title="solution" height={500} width={500} src="https://joy1.videvo.net/videvo_files/video/free/video0467/large_watermarked/_import_61516692993d77.04238324_preview.mp4" /></Tab>
+                    <Tab name="תיאור"> <Likes/> <ImageDisplay imageUrl={imageUrl}/></Tab>
+                    </Tabs>
+                    <Accordion title="דיון">
+תגובה1
+תגובה2
+תגובה3
+</Accordion>
+<Accordion title="שאלות דומות">
+שאלה1
+2שאלה
+שאלה3
+</Accordion>
                 </div>
                 <div>
                     <Editor
@@ -33,6 +56,7 @@ const singleProblem = async ({ params }: PageProps) => {
                         session={undefined}
                     />
                 </div>
+            </div>
             </div>
         </>
     );
---
+++ src/layouts/components/ImageDisplay.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImageDisplay = ({ imageUrl } : any) => {
+  return <img src={imageUrl} alt="Image" />;
+};
+
+export default ImageDisplay;
