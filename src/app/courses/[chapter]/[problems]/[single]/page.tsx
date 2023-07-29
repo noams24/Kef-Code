0343e0256b75af3faf69a0e16aa55a87d5910953
noams24 <noams299@gmail.com>
2023-07-29 12:20:00 +0300

    update

--- src/app/courses/[chapter]/[problems]/[single]/page.tsx
+++ src/app/courses/[chapter]/[problems]/[single]/page.tsx
@@ -28,36 +28,33 @@ const singleProblem = async ({ params }: PageProps) => {
     const imageUrl = "https://i.ibb.co/Gdz4BTg/problem1.png";
     return (
         <>
-        <PageHeader title={params.single}/>
-        <div className="flex justify-center">
-            
-            <div className="text-right flex mt-4">
-            <div className="content">
-                    <Tabs>
-                    <Tab name="פתרונות">כאן יופיעו פתרונות של אנשים</Tab>
-                    <Tab name="פתרון רשמי"><Video title="solution" height={500} width={500} src="https://joy1.videvo.net/videvo_files/video/free/video0467/large_watermarked/_import_61516692993d77.04238324_preview.mp4" /></Tab>
-                    <Tab name="תיאור"> <Likes/> <ImageDisplay imageUrl={imageUrl}/></Tab>
-                    </Tabs>
-                    <Accordion title="דיון">
-תגובה1
-תגובה2
-תגובה3
-</Accordion>
-<Accordion title="שאלות דומות">
-שאלה1
-2שאלה
-שאלה3
-</Accordion>
+            <PageHeader title={params.single} />
+                <div className="text-right flex mt-4 justify-center gap-10">
+                    <div className="content">
+                        <Tabs>
+                            <Tab name="פתרונות">כאן יופיעו פתרונות של אנשים</Tab>
+                            <Tab name="פתרון רשמי"><Video title="solution" height={500} width={500} src="https://joy1.videvo.net/videvo_files/video/free/video0467/large_watermarked/_import_61516692993d77.04238324_preview.mp4" /></Tab>
+                            <Tab name="תיאור"> <Likes /> <ImageDisplay imageUrl={imageUrl} /></Tab>
+                        </Tabs>
+                        <Accordion title="דיון">
+                            תגובה1
+                            תגובה2
+                            תגובה3
+                        </Accordion>
+                        <Accordion title="שאלות דומות">
+                            שאלה1
+                            2שאלה
+                            שאלה3
+                        </Accordion>
+                    </div>
+                    <div>
+                        <Editor
+                            action="פתרון"
+                            content={undefined}
+                            session={undefined}
+                        />
+                    </div>
                 </div>
-                <div>
-                    <Editor
-                        action="פתרון"
-                        content={undefined}
-                        session={undefined}
-                    />
-                </div>
-            </div>
-            </div>
         </>
     );
 };
