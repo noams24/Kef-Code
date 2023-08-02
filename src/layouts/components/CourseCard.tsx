12c741dd34495ea449d93dde484372c15605dd4e
Gal Palas <59319269+GalPalas@users.noreply.github.com>
2023-08-02 11:16:15 +0300

    Update CourseCard.tsx

--- src/layouts/components/CourseCard.tsx
+++ src/layouts/components/CourseCard.tsx
@@ -33,44 +33,8 @@ const CourseCard = ({ title, image, chapters, items, completed }: Props) => {
           <p className="text-xs text-gray-500">פרקים</p>
         </div>
       </div>
-
-
-interface CourseCardProps {
-  title: string;
-  explanation: string;
-  link: string,
-}
-
-const CourseCard: React.FC<CourseCardProps> = ({ title, explanation, link }) => {
-  return (
-    <div className="dark:bg-gray-900 text-center rounded-lg shadow-md p-6 m-1">
-      <h4><Link href={`/courses/${link}`}>{title}</Link></h4>
-      <p>{explanation}</p>
     </div>
   );
 };
 
 export default CourseCard;
-
-{/* 
-<>
-      <SeoMeta
-        title={title}
-        meta_title={meta_title}
-        description={description}
-        image={image}
-      />
-      <PageHeader title="קורסים" />
-      <div className="flex justify-center p-6">
-      <CourseCard
-        title="אלגברה"
-        explanation="אלגברה לינארית 1"
-      />
-      <CourseCard
-        title="אלגוריתמים"
-        explanation="קורס אלגוריתמים"
-      />
-      </div>
-    </>
-
-*/}
