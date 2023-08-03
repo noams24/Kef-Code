3abe60ac06802d5564c8416dd1be6857cfa3709e
noams24 <noams299@gmail.com>
2023-08-03 23:21:50 +0300

    add problem page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,7 +1,15 @@
-const Add = () => {
+
+import { authOptions } from "@/lib/auth";
+import { getServerSession } from "next-auth";
+
+const Add = async () => {
+  const session = await getServerSession(authOptions)
+  if (session?.user.role !== 'ADMIN') {
+    // throw new Error('Unauthorized')
+  }
   return (
     <>
-        
+        You are logged in as {session?.user.role}
     </>
   );
 };
