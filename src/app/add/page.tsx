11a2427b01c779a140612fb4e2320bfd3fa1bf83
noams24 <noams299@gmail.com>
2023-08-03 23:37:44 +0300

    add problem page

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,17 +1,90 @@
-
+import config from "@/config/config.json";
+import { getListPage } from "@/lib/contentParser";
+import PageHeader from "@/partials/PageHeader";
+import SeoMeta from "@/partials/SeoMeta";
+import { RegularPage } from "@/types";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
 
+
 const Add = async () => {
-  const session = await getServerSession(authOptions)
-  if (session?.user.role !== 'ADMIN') {
-    // throw new Error('Unauthorized')
-  }
+  // const session = await getServerSession(authOptions)
+  // if (session?.user.role !== 'ADMIN') {
+  //    throw new Error('Unauthorized')
+  // }
+
+  const data: RegularPage = getListPage("pages/contact.md");
+  const { frontmatter } = data;
+  const { title, description, meta_title, image } = frontmatter;
+  const { contact_form_action } = config.params;
   return (
     <>
-        You are logged in as {session?.user.role}
+      <SeoMeta
+        title={title}
+        meta_title={meta_title}
+        description={description}
+        image={image}
+      />
+      <section className="section-sm">
+        <div className="container">
+          <div className="row">
+            <div className="mx-auto md:col-10 lg:col-6 text-center">
+              <form action={contact_form_action} method="POST">
+                <div className="mb-6">
+                  <label htmlFor="title" className="form-label">
+                    שם השאלה
+                  </label>
+                  <input
+                    id="title"
+                    className="form-input"
+                    type="text"
+                  />
+                </div>
+                <div className="mb-6">
+                  <label htmlFor="course" className="form-label">
+                    קורס
+                  </label>
+                  <input
+                    id="mail"
+                    className="form-input"
+                    type="email"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label htmlFor="chapter" className="form-label">
+                    פרק 
+                  </label>
+                  <input
+                    id="chapter"
+                    className="form-input"
+                    type="chapter"
+                  />
+                </div>
+
+                <div className="mb-6">
+                  <label htmlFor="difficulty" className="form-label">
+                   רמת קושי
+                  </label>
+                  <input
+                    id="difficulty"
+                    className="form-input"
+                    type="difficulty"
+                  />
+                </div>
+
+                <h3 className="mb-10">העלאת תמונה</h3>
+                
+
+                <button type="submit" className="btn btn-primary">
+                  העלאה
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
-
 export default Add;
