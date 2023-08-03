74f6496e07a8d2a455670790000d23035081cbcb
noams24 <noams299@gmail.com>
2023-08-03 16:35:52 +0300

    toolbar

--- src/layouts/components/topBar/TopBar.tsx
+++ src/layouts/components/topBar/TopBar.tsx
@@ -6,21 +6,27 @@ import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { BsList } from "react-icons/bs";
 
 const TopBar = ({ title }: { title: string }) => {
-  return (
-    <section>
-      <div className="text-center mx-auto rounded px-12">
-        <div className="flex justify-between gap-2 rounded bg-gradient-to-b from-body to-theme-light px-8  dark:from-darkmode-body dark:to-darkmode-theme-light">
-          <div className='flex items-center justify-between  rounded h-10 w-24 cursor-pointer'>
-            <FaChevronLeft />
-            <BsList />
-            <FaChevronRight />
-          </div>
-          <Breadcrumbs className="mb-7" />
-          <Timer />
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section>
+            <div className="text-center mx-auto rounded px-12">
+                <div className="flex justify-between gap-2 rounded bg-gradient-to-b from-body to-theme-light px-8  dark:from-darkmode-body dark:to-darkmode-theme-light">
+                    <div className='flex items-center justify-between rounded h-10 w-24 cursor-pointer '>
+                        <div className=" hover:bg-gray-300 hover:border-white">
+                            <FaChevronLeft />
+                        </div>
+                        <div className="rounded hover:bg-gray-300 hover:border-white">
+                            <BsList />
+                        </div>
+                        <div className="rounded hover:bg-gray-300 hover:border-white">
+                            <FaChevronRight />
+                        </div>
+                    </div>
+                    <Breadcrumbs className="mb-7" />
+                    <Timer />
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default TopBar;
