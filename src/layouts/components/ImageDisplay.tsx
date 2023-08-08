fae3673fe805a085ead4eb6508e4e7dbf6737b51
noams24 <noams299@gmail.com>
2023-08-08 22:27:42 +0300

    fixed img h and w

--- src/layouts/components/ImageDisplay.tsx
+++ src/layouts/components/ImageDisplay.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ImageDisplay = ({ imageUrl } : any) => {
-  return <div className="flex justify-center"> <img src={imageUrl} alt="Image" /> </div>;
+  return <div className="flex justify-center max-h-fit max-w-fit"> <img src={imageUrl} alt="Image" /> </div>;
 };
 
 export default ImageDisplay;
