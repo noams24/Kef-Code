eeaa32f72218f6fae421140adb5446d4f08ff190
noams24 <noams299@gmail.com>
2023-08-14 15:47:25 +0300

    solution card

--- src/layouts/components/workSpace/SolutionCard.tsx
+++ src/layouts/components/workSpace/SolutionCard.tsx
@@ -15,22 +15,24 @@ interface SolutionCardProps {
 
 const SolutionCard: React.FC<SolutionCardProps> = ({ author, date, upvotes, comments }) => {
     return (
-        <div className="bg-gray-200 dark:bg-gray-900 dark:text-white rounded-lg shadow p-4 h-20 my-4">
-            <div className="flex justify-center items-center mb-2">
-            <a href={`/authors/${slugify(author)}`} className="font-bold text-center text-lg">
-                <FaRegUserCircle className={"-mt-1 mr-2 inline-block"}/>
-                {humanize(author)}
-                </a>
+        <Link href="/" className="no-underline">
+            <div className="bg-gray-200 dark:bg-gray-900 hover:bg-gray-300 dark:hover:bg-gray-800 dark:text-white rounded-lg shadow p-4 h-20 my-4">
+                <div className="flex justify-center items-center mb-2">
+                    <a href={`/authors/${slugify(author)}`} className="font-bold text-center text-lg hover:text-blue-500 dark:hover:text-blue-500">
+                        <FaRegUserCircle className={"-mt-1 mr-2 inline-block"} />
+                        {humanize(author)}
+                    </a>
                 </div>
-            <div className="flex justify-center items-center text-sm mb-1">
-                <BiUpvote className="mr-1"/>
-                <span className="mr-1">{upvotes}</span>
-                <FaRegComment className="ml-4 mr-1"/>
-                <span>{comments}</span>
-                <AiOutlineCalendar className="ml-4 mr-1"/>
-                <span >{dateFormat(date)}</span>
+                <div className="flex justify-center items-center text-sm mb-1">
+                    <BiUpvote className="mr-1" />
+                    <span className="mr-1">{upvotes}</span>
+                    <FaRegComment className="ml-4 mr-1" />
+                    <span>{comments}</span>
+                    <AiOutlineCalendar className="ml-4 mr-1" />
+                    <span >{dateFormat(date)}</span>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 };
 
