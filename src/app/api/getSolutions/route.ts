b42ba7548c8a2a79e1e539217a198bfaf9861353
noams24 <noams299@gmail.com>
2023-08-21 14:28:08 +0300

    dinamic solutions

--- src/app/api/getSolutions/route.ts
+++ src/app/api/getSolutions/route.ts
@@ -5,40 +5,29 @@ import { db } from '@/lib/db'
 export async function GET(req: Request) {
     const url = new URL(req.url)
     const problemId = Number(url.searchParams.get('problemId'))
-
+    const page = Number(url.searchParams.get('page'))
     if (!problemId) return new Response('Invalid query', { status: 400 })
     try {
         const results = await db.submissions.findMany({
             orderBy: {
                 createdAt: 'desc',
-              },
+            },
             where: {
                 problemId,
-                isPublic:true
+                isPublic: true
             },
             include: {
-                user:true
-            }
+                user: true,
+                votes: true,
+                comments: true
+            },
+            take:5,
+            skip: (page - 1) * 5
         })
-        if (!results) return new Response('No results', {status: 401})
 
-        const likes: number = await db.vote.count({
-            where: {
-                SubmissionId: results[0].id,
-                type: 'LIKE'
-            }
-        });
-
-        const comments: number = await db.vote.count({
-            where: {
-                SubmissionId: results[0].id,
-            }
-        });
-
-        const { createdAt, user: { name, image } } = results[0];
-        const extractedData = {createdAt,name,image, likes, comments};
-        console.log(extractedData)
-        return new Response(JSON.stringify(extractedData))
+        // console.log(results)
+        if (!results) return new Response('No results', { status: 401 })
+        return new Response(JSON.stringify(results))
     }
     catch (error) {
         return new Response('Could not fetch posts', { status: 500 })
--- src/layouts/components/workSpace/solutionSection/SolutionsSection.tsx
+++ src/layouts/components/workSpace/solutionSection/SolutionsSection.tsx
@@ -4,87 +4,107 @@ import * as React from "react"
 import PaginationControls from './PaginationControl'
 import { useGenerationStoree } from '@/store/store';
 import {
-    Select,
-    SelectContent,
-    SelectGroup,
-    SelectItem,
-    SelectLabel,
-    SelectTrigger,
-    SelectValue,
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/Select"
 import axios, { AxiosError } from 'axios'
 import { useQuery } from '@tanstack/react-query'
 import { useGenerationStore } from '@/store/store';
+import { Item } from '@radix-ui/react-dropdown-menu';
 interface SolutionSectionProps {
-    problemId: String
+  problemId: String
 }
 
 
-const Mockdata = [
-    {author: "ישראל ישראלי", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "ג'ון סינה", date: "2023-08-14", likes: 18, comments: 4},
-    {author: "מסי", date: "2023-08-14", likes: 9, comments: 2},
-    {author: "משתמש24", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "משה זוכמיר", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "יוני חטאים", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "אנונימי", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "אלון מאסק", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "סנופ דוג", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "משתמש5", date: "2023-08-14", likes: 42, comments: 7},
-    {author: "משתמש21", date: "2023-08-14", likes: 42, comments: 7}
+
+
+
+const SolutionsSection: React.FC<SolutionSectionProps> = ({ problemId }) => {
+  const { page, setPage } = useGenerationStoree()
+  const per_page = 5
+
+  // mocked, skipped and limited in the real app
+  const start = (Number(page) - 1) * Number(per_page) // 0, 5, 10 ...
+  const end = start + Number(per_page) // 5, 10, 15 ...
+  // const { solutionState, setSolution } = useGenerationStore()
+
+  const Mockdata = [
+    { author: "ישראל ישראלי", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "ג'ון סינה", date: "2023-08-14", likes: 18, comments: 4 },
+    { author: "מסי", date: "2023-08-14", likes: 9, comments: 2 },
+    { author: "משתמש24", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "משה זוכמיר", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "יוני חטאים", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "אנונימי", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "אלון מאסק", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "סנופ דוג", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "משתמש5", date: "2023-08-14", likes: 42, comments: 7 },
+    { author: "משתמש21", date: "2023-08-14", likes: 42, comments: 7 }
   ]
+  const slicedData = Mockdata.slice(start, end)
 
-
-  const SolutionsSection: React.FC<SolutionSectionProps> = ({ problemId }) => {
-    const { page, setPage } = useGenerationStoree()
-    const per_page = 5
-  
-    // mocked, skipped and limited in the real app
-    const start = (Number(page) - 1) * Number(per_page) // 0, 5, 10 ...
-    const end = start + Number(per_page) // 5, 10, 15 ...
-    const { solutionState, setSolution } = useGenerationStore()
-
+  const develop = process.env.DATABASE_URL !== undefined && process.env.DATABASE_URL !== null;
   //get data from the db
-//   const { isFetching, data, refetch, isFetched, isLoading: isLoadingData } = useQuery({
-//     queryFn: async () => {
-//       const query = `/api/getSolutions?problemId=${problemId}`
-//       const { data } = await axios.get(query)
-//       return data
-//     },
-//   })
-
-//   console.log(data)
-
-    const entries = Mockdata.slice(start, end)
-    return (
-        <>
-        <div className="mt-3 dark:text-white text-center" dir="rtl">
-            <Select>
-                <SelectTrigger className="w-[180px]">
-                    <SelectValue placeholder="לייקים" />
-                </SelectTrigger>
-                <SelectContent>
-                    <SelectGroup>
-                        <SelectLabel>מיין לפי</SelectLabel>
-                        <SelectItem value="likes">לייקים</SelectItem>
-                        <SelectItem value="recent">נוסף לאחרונה</SelectItem>
-                    </SelectGroup>
-                </SelectContent>
-            </Select>
-            </div>
-            {entries.map((item, index) => (
-      <div key={index}>
-        <SolutionCard author={item.author} date={item.date} likes={item.likes} comments={item.comments} />
+  
+  // if (!develop) {
+    const { isFetching, data, refetch, isFetched, isLoading: isLoadingData } = useQuery({
+      queryKey: ['solutions', page],
+      queryFn: async () => {
+        const query = `/api/getSolutions?problemId=${problemId}&page=${page}`
+        const { data } = await axios.get(query)
+        return data
+      },
+      keepPreviousData: true
+    })
+  //}
+ 
+  return (
+    <>
+      <div className="mt-3 dark:text-white text-center" dir="rtl">
+        <Select>
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="לייקים" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>מיין לפי</SelectLabel>
+              <SelectItem value="likes">לייקים</SelectItem>
+              <SelectItem value="recent">נוסף לאחרונה</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+      {/* {entries.map((item, index) => (
+        <div key={index}>
+          <SolutionCard author={item.author} date={item.date} likes={item.likes} comments={item.comments} />
         </div>
-        // <p key={entry}>{entry}</p>
-      ))}
+      ))} */}
+
+      {develop ? <div>{slicedData.map((item, index) => (
+        <div key={index}>
+          <SolutionCard author={item.author} date={item.date} likes={item.likes} comments={item.comments} />
+        </div>
+      ))} </div>
+
+        : <div>
+          {data?.map((item: any, index: any) => (
+            <div key={index}>
+              <SolutionCard author={item.user.username} date={item.createdAt} likes={item.votes.length} comments={item.comments.length} />
+            </div>
+          ))}</div>}
+
       <PaginationControls
-        hasNextPage={end < Mockdata.length}
+        hasNextPage={end < data.length}
         hasPrevPage={start > 0}
-        numberOfItems= {Mockdata.length}
+        numberOfItems={data.length}
       />
-        </>
-    );
+    </>
+  );
 };
 
 export default SolutionsSection;
