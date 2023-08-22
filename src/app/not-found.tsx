a0d87b1fb1bc6bf31b7697b68f38a5a60f57fba8
noams24 <noams299@gmail.com>
2023-08-22 15:57:59 +0300

    fixing bug

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,18 +7,19 @@ import { RegularPage } from "@/types";
 import Link from "next/link";
 
 const NotFound = () => {
-  const data: RegularPage = getListPage("pages/404.md");
-  const { image, title } = data.frontmatter;
+  // const data: RegularPage = getListPage("pages/404.md");
+  // const { image, title } = data.frontmatter;
+  const title = "דף לא נמצא"
   return (
     <>
-      <SeoMeta title={"Page Not Found"} image={"/images/404.png"} />
+      <SeoMeta title={"דף לא נמצא"} image={"/images/404.png"} />
       <section className="section-sm">
         <div className="container">
           <div className="row justify-center">
             <div className="text-center sm:col-10 md:col-8 lg:col-6">
               <ImageFallback
                 className="mb-8 w-full"
-                src={image}
+                src="/images/404.png"
                 alt="page not found"
                 height={320}
                 width={630}
@@ -28,10 +29,10 @@ const NotFound = () => {
                 dangerouslySetInnerHTML={markdownify(title)}
               ></h1>
               <div className="content">
-                <MDXContent content={data.content} />
+                <MDXContent content={'דף לא נמצא'} />
               </div>
               <Link href="/" className="btn btn-primary mt-8">
-                Back To Home
+                חזרה לדף הבית
               </Link>
             </div>
           </div>
