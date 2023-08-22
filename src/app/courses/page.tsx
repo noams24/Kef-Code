35a568b90f04c499f4c44aad692f15889e0ee701
noams24 <noams299@gmail.com>
2023-08-22 15:50:20 +0300

    fixing bug

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,7 +1,7 @@
 import Testimonials from "@/partials/Testimonials";
 import { getListPage } from "@/lib/contentParser";
 
-const Courses = async () => {
+const Courses = () => {
   const testimonial1 = getListPage("sections/CS.md");
   const testimonial2 = getListPage("sections/Math.md");
   const testimonial3 = getListPage("sections/Other.md");
