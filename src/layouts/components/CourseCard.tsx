36fbe0e15dcc59638a812fadf5896ae3bc535e9b
hilma <shiral@hilma.tech>
2023-09-02 21:19:38 +0300

    fix card course text

--- src/layouts/components/CourseCard.tsx
+++ src/layouts/components/CourseCard.tsx
@@ -12,32 +12,34 @@ interface Props {
 const CourseCard = ({ title, link, image, chapters, items, completed }: Props) => {
   return (
     <a href={`courses/${link}`}>
-    <div className="max-w-xs cursor-pointer rounded border mx-2 my-2 inline-block hover:shadow-lg">
-      <div className="relative w-full">
-        
-        <img
-          className="w-64 h-64 object-cover rounded"
-          src={image}
-          alt="Sunset in the mountains"
-        />
-        
-        <div className="absolute top-5 left-5">
-          <h2 className="text-2xl text-white mb-24 w-64 ">{title}</h2>
-          <Pi completed={completed} />
-        </div>
-      </div>
+      <div className="max-w-xs cursor-pointer rounded border mx-2 my-2 inline-block hover:shadow-lg">
+        <div className="relative w-full">
 
-      <div className="flex px-6 py-4 justify-end items-center gap-x-4">
-        <div className="flex-col text-center">
-          <p className="text-2xl">{items}</p>
-          <p className="text-xs text-gray-500">שאלות</p>
+          <img
+            className="w-64 h-64 object-cover rounded"
+            src={image}
+            alt="Sunset in the mountains"
+          />
+
+          <div className="absolute top-5 left-5 right-5">
+            <h2 className="course-card-title" dir="rtl">
+              {title}
+            </h2>
+            <Pi completed={completed} />
+          </div>
         </div>
-        <div className="flex-col text-center">
-          <p className="text-2xl">{chapters}</p>
-          <p className="text-xs text-gray-500">פרקים</p>
+
+        <div className="flex px-6 py-4 justify-end items-center gap-x-4">
+          <div className="flex-col text-center">
+            <p className="text-2xl">{items}</p>
+            <p className="text-xs text-gray-500">שאלות</p>
+          </div>
+          <div className="flex-col text-center">
+            <p className="text-2xl">{chapters}</p>
+            <p className="text-xs text-gray-500">פרקים</p>
+          </div>
         </div>
       </div>
-    </div>
     </a>
   );
 };
--- src/layouts/components/Pi.tsx
+++ src/layouts/components/Pi.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 function Pi({ completed }: Props) {
   return (
-    <svg viewBox="0 0 36 36" className="circular-chart green">
+    <svg viewBox="0 0 36 36" className="circular-chart green mt-24">
       <path
         className="circle-bg"
         d="M18 2.0845
