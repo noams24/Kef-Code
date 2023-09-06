67799ee957ad194e082a0be67ccaf1c620287826
noams24 <noams299@gmaill.com>
2023-09-06 15:12:28 +0300

    status

--- src/layouts/components/Status.tsx
+++ src/layouts/components/Status.tsx
@@ -1,3 +1,4 @@
+'use client'
 
 import {
   Select,
@@ -8,17 +9,61 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/Select";
-const Status = () => {
+import { useMutation } from '@tanstack/react-query'
+import axios, { AxiosError } from 'axios'
+import { useCustomToasts } from '@/hooks/use-custom-toast'
+import { toast } from '@/hooks/use-toast'
+import { useQuery } from '@tanstack/react-query'
+import { useDevelop } from '@/store/store'
+
+const Status = ({problemId}:any) => {
+  
+  const { loginToast } = useCustomToasts()
+  const { development } = useDevelop()
+  // console.log(problemId)
+  const { data: previousStatus } = useQuery({
+    queryKey: ['status'],
+    queryFn: async () => {
+        if (development) return null
+        const query = `/api/getStatus?problemId=${problemId}`
+        const { data } = await axios.get(query)
+        return data
+    },
+},)
+
+  const { mutate: handleClick} = useMutation({
+    mutationFn: async (status : any) => {
+      const payload: any = { problemId, status }
+      const { data } = await axios.post('/api/setStatus', payload)
+      return data
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 401) {
+          return loginToast()
+        }
+      }
+      toast({
+        title: 'There was an error.',
+        description: 'Could not create subreddit.',
+        variant: 'destructive',
+      })
+    },
+    onSuccess: () => {
+    },
+  })
+  console.log(previousStatus)
   return (
     <div>
-      <Select>
+      {previousStatus &&
+       <Select defaultValue={previousStatus.status} onValueChange={(status) => (handleClick(status))}>
         <SelectTrigger className="w-[100px]">
           <SelectValue placeholder="סטטוס" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
             <SelectLabel dir="rtl">הגדר סטטוס</SelectLabel>
-            <SelectItem dir="rtl" value="START">
+            <SelectItem dir="rtl" value="BEGIN">
               עוד לא התחלתי
             </SelectItem>
             <SelectItem dir="rtl" value="ONGOING">
@@ -32,7 +77,7 @@ const Status = () => {
             </SelectItem>
           </SelectGroup>
         </SelectContent>
-      </Select>
+      </Select>}
     </div>
     // <QuestionMarkCircledIcon width={25} height={25}/>
   );
