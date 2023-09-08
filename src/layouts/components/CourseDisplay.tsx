b21c634f2f761045a2b8deb83faf54936aa59c05
noams24 <noams299@gmaill.com>
2023-09-08 18:13:34 +0300

    course progress pie backend

--- src/layouts/components/CourseDisplay.tsx
+++ src/layouts/components/CourseDisplay.tsx
@@ -3,6 +3,10 @@
 import CourseCard from "@/components/CourseCard";
 import { Course } from "@/types";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import { useDevelop } from '@/store/store'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import Pi from "./Pi"
 
 interface PageData {
   notFound?: boolean;
@@ -15,7 +19,7 @@ interface PageData {
   };
 }
 
-const CourseDisplay = ({ data }: { data: PageData }) => {
+const CourseDisplay = ({ data, session }: { data: PageData, session:any }) => {
   const idSelector = `slider-${data.frontmatter.title}`;
 
   const slideLeft = () => {
@@ -27,6 +31,18 @@ const CourseDisplay = ({ data }: { data: PageData }) => {
     let slider: any = document.getElementById(idSelector);
     slider.scrollLeft = slider.scrollLeft + 500;
   };
+
+  const { development } = useDevelop()
+
+  const { data: coursePercent } = useQuery({
+    queryKey: ['coursePercent'],
+    queryFn: async () => {
+        if (development || !session) return null
+        const query = `/api/getCoursePercent`
+        const { data } = await axios.get(query)
+        return data
+    },
+},)
 
   return (
     <>
@@ -44,7 +60,7 @@ const CourseDisplay = ({ data }: { data: PageData }) => {
           className="w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide"
         >
           {data.frontmatter.courses?.map(
-            ({ title, link, image, chapters, items, completed }) => (
+            ({ title, link, image, chapters, items }) => (
               <CourseCard
                 key={title}
                 link={link}
@@ -52,8 +68,9 @@ const CourseDisplay = ({ data }: { data: PageData }) => {
                 image={image}
                 chapters={chapters}
                 items={items}
-                completed={completed}
-              />
+              >
+                {!session ? null : ((coursePercent && coursePercent[link]) ? <Pi completed={String(coursePercent[link])} /> : <Pi completed={'0'} />)}
+              </CourseCard>
             ),
           )}
         </div>
