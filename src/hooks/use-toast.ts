5adc0cde82263f27e1db190f0a3addc53cb1e6f7
noams24 <noams299@gmaill.com>
2023-09-16 18:40:53 +0300

    fix: Toast component

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -2,8 +2,8 @@ import * as React from "react"
 
 import type { ToastActionElement, ToastProps } from "@/components/ui/Toast"
 
-const TOAST_LIMIT = 1
-const TOAST_REMOVE_DELAY = 1000000
+const TOAST_LIMIT = 3
+const TOAST_REMOVE_DELAY = 500
 
 type ToasterToast = ToastProps & {
   id: string
