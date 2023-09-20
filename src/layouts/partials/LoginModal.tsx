9163418ffd27de14625b051fd1bc0c9164034d2a
noams24 <noams299@gmaill.com>
2023-09-20 21:00:30 +0300

    enhancment: Modal

--- src/layouts/partials/LoginModal.tsx
+++ src/layouts/partials/LoginModal.tsx
@@ -37,7 +37,7 @@ const LoginModal: FC = () => {
       {!isOpen ? null : (
         <div className="fixed inset-0 bg-zinc-900/20 z-10">
           <div className="container flex items-center h-full max-w-lg mx-auto">
-            <div className="relative bg-white dark:bg-black w-full h-fit py-20 px-2 rounded-lg">
+            <div className="relative bg-white dark:bg-darkmode-body w-full h-fit py-20 px-2 rounded-lg">
               <div className="absolute top-4 right-4">
                 {/* Close Modal Button */}
                 <Button
