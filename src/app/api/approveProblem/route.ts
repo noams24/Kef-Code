96821ade71f5d2e776e7b8657207850684683329
noams24 <noams299@gmail.com>
2023-11-02 17:19:16 +0200

    enhancement: admin can approve problems

---
+++ src/app/api/approveProblem/route.ts
@@ -0,0 +1,45 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+// import { z } from 'zod'
+
+export async function POST(req: Request) {
+  try {
+    const session = await getAuthSession();
+    if (!session || session.user.role !== "ADMIN")
+      return new Response("Unauthorized", { status: 401 });
+
+    const body = await req.json();
+    const id = body.data.id;
+    const course = body.data.course;
+    const chapter = body.data.chapter;
+    const title = body.data.title;
+    const difficulty = body.data.difficulty;
+    const img = body.data.img;
+    const status = body.status;
+
+    // delete it from need verified table
+    await db.problemsneedverify.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    if (status === "approve") {
+      await db.problem.create({
+        data: {
+          title: title,
+          course: course,
+          chapter: chapter,
+          difficulty: difficulty,
+          img: img,
+        },
+      });
+    }
+
+    return new Response("OK");
+  } catch (error) {
+    return new Response("Could not create problem. Please try later", {
+      status: 500,
+    });
+  }
+}
--- src/app/approveproblems/page.tsx
+++ src/app/approveproblems/page.tsx
@@ -1,10 +1,7 @@
 import { db } from "@/lib/db";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
-import { AiOutlineCheck, AiOutlineClose } from "react-icons/ai";
-import { UserAvatar } from "@/components/UserAvatar";
-import { humanize, slugify } from "@/lib/utils/textConverter";
-import hebrewDateFormat from "@/lib/utils/hebrewDateFormat";
+import { ApproveProblem } from "./ApproveProblem";
 
 const approveproblem = async () => {
   const session = await getServerSession(authOptions);
@@ -14,51 +11,10 @@ const approveproblem = async () => {
 
   const problems: any = await db.problemsneedverify.findMany({
     include: {
-        user: true,
-    }
+      user: true,
+    },
   });
-    // console.log(problems);
-  return (
-    <>
-      <div dir="rtl" className="py-3 md:mx-64 lg:mx-96 items-center">
-        {problems.map((item: any) => (
-          <div key={item.id} className="border-2 shadow-lg rounded-lg mb-9">
-            <div className="flex">
-          <UserAvatar
-            user={{ name: item.user.username || null, image: item.user.image || null }}
-            className="h-12 w-12"
-          />
-          <div className="mr-1">
-            <a
-              href={`/authors/${slugify(item.user.username)}`}
-              className="mr-4 font-bold text-center text-lg hover:text-blue-500 dark:hover:text-blue-500"
-            >
-              {humanize(item.user.username)}
-            </a>
-            <div className="flex">
-              <span className="mr-4">{hebrewDateFormat(item.createdAt)}</span>
-            </div>
-          </div>
-        </div>
-            <img src={item.img} alt={item.title} className="w-42 h-42" />
-            <p>קורס: {item.course} </p>
-            <p>פרק: {item.chapter}</p>
-            <p>שם השאלה: {item.title}</p>
-            <p>רמת קושי: {item.difficulty}</p>
-            <div className="flex justify-center gap-3">
-              <button>
-                <AiOutlineClose className="h-8 w-8 text-red-500"/>
-                <h5>ביטול</h5>
-              </button>
-              <button>
-                <AiOutlineCheck className="h-8 w-8 text-green-500"/>
-                <h5>אישור</h5>
-              </button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+  return <>{problems && <ApproveProblem data={problems} />}</>;
 };
+
 export default approveproblem;
