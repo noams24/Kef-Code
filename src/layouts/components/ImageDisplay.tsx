e67b02346745a53a214fc4d161579e64f6bcb381
noams24 <noams299@gmail.com>
2023-11-13 18:17:29 +0200

    responssive image

--- src/layouts/components/ImageDisplay.tsx
+++ src/layouts/components/ImageDisplay.tsx
@@ -1,7 +1,11 @@
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import Image from "next/image";
 
 const ImageDisplay = ({ imageUrl }: any) => {
   return (
+    <TransformWrapper
+    >
+      <TransformComponent >
     <Image
       className="dark:invert object-cover rounded-md transition-opacity opacity-0 duration-[1s]"
       height={500}
@@ -10,6 +14,8 @@ const ImageDisplay = ({ imageUrl }: any) => {
       alt="Image"
       onLoadingComplete={(image) => image.classList.remove("opacity-0")}
     />
+      </TransformComponent>
+    </TransformWrapper>
   );
 };
 
