9ec8b4bbb82c856e566d4cd7495576735c7e8b30
noams24 <noams299@gmail.com>
2023-12-26 15:39:58 +0200

    styling: solution card

--- src/layouts/components/workSpace/solutionSection/SolutionCard.tsx
+++ src/layouts/components/workSpace/solutionSection/SolutionCard.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React from "react";
-import { FaRegComment, FaRegUserCircle } from "react-icons/fa";
+import { FaRegComment} from "react-icons/fa";
 import { humanize, slugify } from "@/lib/utils/textConverter";
 import hebrewDateFormat from "@/lib/utils/hebrewDateFormat";
 import { AiOutlineCalendar, AiFillLike } from "react-icons/ai";
 import { UserAvatar } from "@/components/UserAvatar";
 import Link from "next/link";
+
+import { Card, CardActionArea } from "@mui/material";
 
 interface SolutionCardProps {
   author: string;
@@ -24,35 +26,35 @@ const SolutionCard: React.FC<SolutionCardProps> = ({
   avatar,
 }) => {
   return (
-    <div
-      dir="rtl"
-      className="first-letter:bg-gray-200 dark:bg-gray-900 hover:bg-gray-300 dark:hover:bg-gray-800 dark:text-white hover:cursor-pointer rounded-lg shadow p-4 h-20 my-4 no-underline"
-    >
-      <div className="flex justify-center items-center mb-2">
-        <Link
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-          href={`/user/${slugify(author)}`}
-          target="_blank"
-          className="font-bold font-primary text-center text-lg hover:text-blue-500 dark:hover:text-blue-500"
-        >
-          {/* <FaRegUserCircle className={"-mt-1 ml-2 inline-block"} /> */}
-          <UserAvatar
-            user={{ name: "" || null, image: avatar || null }}
-            className="h-7 w-7 -mt-1 ml-2 inline-block"
-          />
-          {humanize(author)}
-        </Link>
-      </div>
-      <div className="flex justify-center items-center text-sm mb-1">
-        <span>{hebrewDateFormat(date)}</span>
-        <AiOutlineCalendar className="ml-4 mr-1" />
-        <span>{comments}</span>
-        <FaRegComment className="ml-4 mr-1" />
-        <span className="mr-1">{likes}</span>
-        <AiFillLike className="mr-1" />
-      </div>
+    <div dir="rtl" className="p-4 h-20 my-4">
+      <Card className="border border-gray-200 dark:border-gray-500">
+        <CardActionArea>
+          <div className="flex justify-center m-2">
+            <UserAvatar
+              user={{ name: "" || null, image: avatar || null }}
+              className="h-8 w-8 ml-2 inline-block"
+            />
+            <Link
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+              href={`/user/${slugify(author)}`}
+              target="_blank"
+              className="pt-1 font-bold font-primary text-center text-lg hover:text-blue-500 dark:hover:text-blue-500"
+            >
+              {humanize(author)}
+            </Link>
+          </div>
+          <div className="flex justify-center items-center text-sm mb-1">
+            <span>{hebrewDateFormat(date)}</span>
+            <AiOutlineCalendar className="ml-4 mr-1" />
+            <span>{comments}</span>
+            <FaRegComment className="ml-4 mr-1" />
+            <span className="mr-1">{likes}</span>
+            <AiFillLike className="mr-1" />
+          </div>
+        </CardActionArea>
+      </Card>
     </div>
   );
 };
