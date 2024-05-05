9c41a5168b47cd7683b8e4f9f3a212d7733e0f33
noams24 <noams299@gmail.com>
2024-05-05 18:50:40 +0300

    update lexical

--- src/layouts/editor/nodes/StickyNode/StickyComponent.tsx
+++ src/layouts/editor/nodes/StickyNode/StickyComponent.tsx
@@ -1,63 +1,60 @@
-"use client"
+"use client";
 import {
   NodeKey,
   LexicalEditor,
-  $getSelection,
   SELECTION_CHANGE_COMMAND,
-} from 'lexical';
-import { useLexicalNodeSelection } from '@lexical/react/useLexicalNodeSelection';
-import { mergeRegister } from '@lexical/utils';
-import {
   COMMAND_PRIORITY_LOW,
-} from 'lexical';
-
-
-import './StickyNode.css';
-
-import { useLexicalComposerContext } from '@lexical/react/LexicalComposerContext';
-import {
   $getNodeByKey,
-} from 'lexical';
-import { useEffect, useRef, useState, lazy } from 'react';
+  $setSelection,
+} from "lexical";
+import { useLexicalNodeSelection } from "@lexical/react/useLexicalNodeSelection";
+import { mergeRegister } from "@lexical/utils";
+import "./StickyNode.css";
 
-import FormatPaintIcon from '@mui/icons-material/FormatPaint';
-import DeleteIcon from '@mui/icons-material/Delete';
-import DragIndicatorIcon from '@mui/icons-material/DragIndicator';
-import IconButton from '@mui/material/IconButton';
+import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
+import { useEffect, useRef, useState } from "react";
+import { editorConfig } from "./config";
+import dynamic from "next/dynamic";
+import { IconButton } from "@mui/material";
+import { Delete, FormatPaint, DragIndicator } from "@mui/icons-material";
+import { $isStickyNode } from ".";
 
-const NestedEditor = lazy(() => import('../../NestedEditor'));
+const NestedEditor = dynamic(() => import("@/layouts/editor/NestedEditor"), {
+  ssr: false,
+});
 
-export default function StickyComponent({ nodeKey, color, stickyEditor }: { stickyEditor: LexicalEditor; color: 'pink' | 'yellow'; nodeKey: NodeKey; }): JSX.Element {
+export default function StickyComponent({
+  nodeKey,
+  color,
+  stickyEditor,
+}: {
+  stickyEditor: LexicalEditor;
+  color: "pink" | "yellow";
+  nodeKey: NodeKey;
+}): JSX.Element {
   const [editor] = useLexicalComposerContext();
-  const [isSelected, setSelected, clearSelection] = useLexicalNodeSelection(nodeKey);
-  const [isDraggable, setDraggable] = useState(false);
+  const [isSelected, setSelected] = useLexicalNodeSelection(nodeKey);
 
   const stickyContainerRef = useRef<null | HTMLDivElement>(null);
 
-
   useEffect(() => {
-    mergeRegister(
-      stickyEditor.registerCommand(
+    return mergeRegister(
+      editor.registerCommand(
         SELECTION_CHANGE_COMMAND,
-        () => {
-          const selection = $getSelection();
-          if (selection) {
-            clearSelection();
-            setSelected(true);
-            return true;
-          }
-          return false;
+        (_, activeEditor) => {
+          if (activeEditor !== stickyEditor) clearSelection();
+          return true;
         },
         COMMAND_PRIORITY_LOW,
       ),
     );
-  }, [
-    clearSelection,
-    stickyEditor,
-    isSelected,
-    nodeKey,
-    setSelected,
-  ]);
+  }, [isSelected]);
+
+  const clearSelection = () => {
+    stickyEditor.update(() => {
+      $setSelection(null);
+    });
+  };
 
   const handleDelete = () => {
     editor.update(() => {
@@ -71,39 +68,67 @@ export default function StickyComponent({ nodeKey, color, stickyEditor }: { stic
   const handleColorChange = () => {
     editor.update(() => {
       const node = $getNodeByKey(nodeKey);
-      if (node) {
-        node.toggleColor();
-      }
+      if (!$isStickyNode(node)) return;
+      node.toggleColor();
     });
   };
 
   const onChange = () => {
     editor.update(() => {
       const node = $getNodeByKey(nodeKey);
-      if (node) {
-        node.setEditor(stickyEditor);
-      }
+      if (!$isStickyNode(node)) return;
+      node.setEditor(stickyEditor);
     });
-  }
+  };
 
   return (
-    <div ref={stickyContainerRef} className={"sticky-note-container" + (isSelected ? " selected" : "")} draggable={isDraggable} {...{ theme: 'light' }}>
-      <div className='sticky-tools'>
-        <IconButton sx={{ displayPrint: 'none' }} onClick={handleDelete} aria-label="Delete sticky note" title="Delete" color='inherit' size='small'>
-          <DeleteIcon fontSize='inherit' />
+    <div
+      ref={stickyContainerRef}
+      className="sticky-note-container"
+      draggable={isSelected}
+      {...{ theme: "light" }}
+    >
+      <div className="sticky-tools">
+        <IconButton
+          sx={{ displayPrint: "none" }}
+          onClick={handleDelete}
+          aria-label="Delete sticky note"
+          title="Delete"
+          color="inherit"
+          size="small"
+        >
+          <Delete fontSize="inherit" />
         </IconButton>
-        <IconButton sx={{ displayPrint: 'none' }} color='inherit' size='small' aria-label="Change sticky note color" title="Color" onClick={handleColorChange}>
-          <FormatPaintIcon fontSize='inherit' />
+        <IconButton
+          sx={{ displayPrint: "none" }}
+          color="inherit"
+          size="small"
+          aria-label="Change sticky note color"
+          title="Color"
+          onClick={handleColorChange}
+        >
+          <FormatPaint fontSize="inherit" />
         </IconButton>
-        {isSelected && <IconButton className='drag-btn' sx={{ displayPrint: 'none', mr: "auto" }} color='inherit' size='small' aria-label="Drag sticky note" title="Drag"
-          onMouseDown={() => setDraggable(true)} onMouseUp={() => setDraggable(false)}>
-          <DragIndicatorIcon fontSize='inherit' />
+        <IconButton
+          className="drag-btn"
+          sx={{ displayPrint: "none", mr: "auto" }}
+          color="inherit"
+          size="small"
+          aria-label="Drag sticky note"
+          title="Drag"
+          onMouseDown={() => setSelected(true)}
+          onMouseUp={() => setSelected(false)}
+        >
+          <DragIndicator fontSize="inherit" />
         </IconButton>
-        }
       </div>
       <div className={`sticky-note ${color}`}>
-        <NestedEditor initialEditor={stickyEditor} onChange={onChange} />
+        <NestedEditor
+          initialEditor={stickyEditor}
+          initialNodes={editorConfig.nodes}
+          onChange={onChange}
+        />
       </div>
-    </div >
+    </div>
   );
 }
