d74dcdea593a86b5239dbef4dbd1c62c6ff69490
noams24 <noams299@gmail.com>
2025-05-09 16:57:04 +0300

    change mega link

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,30 +1,30 @@
-"use client";
+'use client';
 
-import ImageFallback from "@/helpers/ImageFallback";
-import Footer from "@/partials/Footer";
-import SeoMeta from "@/partials/SeoMeta";
-import { FaCheck } from "react-icons/fa/index.js";
-import Link from "next/link";
+import ImageFallback from '@/helpers/ImageFallback';
+import Footer from '@/partials/Footer';
+import SeoMeta from '@/partials/SeoMeta';
+import { FaCheck } from 'react-icons/fa/index.js';
+import Link from 'next/link';
 // import Image from "next/image";
-import Features from "@/components/Features";
-import Editor from "@/layouts/editor/components/Editor";
+import Features from '@/components/Features';
+import Editor from '@/layouts/editor/components/Editor';
 // import playgroundTemplate from "./jsonFiles/empty.json";
-import playgroundTemplate from "@/components/workSpace/jsonFiles/empty.json";
-import { EditorDocument } from "@/components/workSpace/types";
+import playgroundTemplate from '@/components/workSpace/jsonFiles/empty.json';
+import { EditorDocument } from '@/components/workSpace/types';
 // import Tilt from "react-parallax-tilt";
-import TiltedImage from "@/components/TiltedImage";
-import BubbleText from "@/components/BubbleText";
-import { useRef } from "react";
-import { useIsVisible } from "@/helpers/Visible";
+import TiltedImage from '@/components/TiltedImage';
+import BubbleText from '@/components/BubbleText';
+import { useRef } from 'react';
+import { useIsVisible } from '@/helpers/Visible';
 
 const Home = () => {
-  const ul1 = "מגוון שאלות ממבחנים עם פתרונות הלקוחות ";
-  const ul2 = "אינטרקציה עם ";
-  const ul3 = " על מנת לכתוב נוסחאות מתמטיות";
+  const ul1 = 'מגוון שאלות ממבחנים עם פתרונות הלקוחות ';
+  const ul2 = 'אינטרקציה עם ';
+  const ul3 = ' על מנת לכתוב נוסחאות מתמטיות';
   // const ul4 = "גרפים: אינטרקציה עם ";
-  const ul5 = " על מנת לשרטט גרפים וצורות";
+  const ul5 = ' על מנת לשרטט גרפים וצורות';
   // const ul6 = "איור: אינטרקציה עם ";
-  const ul7 = " על מנת לאייר כמו כתב יד";
+  const ul7 = ' על מנת לאייר כמו כתב יד';
 
   const document = playgroundTemplate as unknown as EditorDocument;
 
@@ -36,9 +36,9 @@ const Home = () => {
   return (
     <>
       <SeoMeta />
-      <section className="bg-gradient2 sm:h-[90dvh] md:h-[92dvh] flex items-center">
+      <section className="bg-gradient2 flex items-center sm:h-[90dvh] md:h-[92dvh]">
         <div className="container">
-          <div className="flex sm:flex-wrap-reverse md:flex-nowrap justify-center gap-x-3 animate-slidein opacity-0 items-center md:pb-10">
+          <div className="flex animate-slidein items-center justify-center gap-x-3 opacity-0 sm:flex-wrap-reverse md:flex-nowrap md:pb-10">
             <TiltedImage />
             <div className="max-w-xs md:mb-5">
               <BubbleText />
@@ -48,7 +48,7 @@ const Home = () => {
               </p>
               <div className="flex justify-center">
                 <Link
-                  className="ml-8 btn btn-start transition-all shadow-[3px_3px_0px_black] hover:shadow-none hover:translate-x-[3px] hover:translate-y-[3px]"
+                  className="btn btn-start ml-8 shadow-[3px_3px_0px_black] transition-all hover:translate-x-[3px] hover:translate-y-[3px] hover:shadow-none"
                   href="/courses"
                 >
                   התחלה
@@ -62,8 +62,8 @@ const Home = () => {
       <section className="section-sm bg-gradient">
         <div
           ref={ref2}
-          className={`container transition-opacity ease-in duration-1000 ${
-            isVisible2 ? "opacity-100" : "opacity-0"
+          className={`container transition-opacity duration-1000 ease-in ${
+            isVisible2 ? 'opacity-100' : 'opacity-0'
           }`}
         >
           <div className="row items-center justify-between">
@@ -75,17 +75,17 @@ const Home = () => {
                 alt="מה כלול בכיף קוד"
               />
             </div>
-            <div className="md:col-7 lg:col-6 ">
+            <div className="md:col-7 lg:col-6">
               <h2 className="mb-4 text-center">מה כלול </h2>
               <p className="mb-8 text-lg" />
               <ul dir="rtl">
-                <div className="flex gap-4 items-center mb-3">
+                <div className="mb-3 flex items-center gap-4">
                   <div>
                     <FaCheck />
                   </div>
                   <li> מעל 20 קורסים שונים</li>
                 </div>
-                <div className="flex gap-4 items-center mb-3">
+                <div className="mb-3 flex items-center gap-4">
                   <div>
                     <FaCheck />
                   </div>
@@ -93,20 +93,20 @@ const Home = () => {
                     {ul1}
                     <Link
                       className="text-blue-500 hover:underline"
-                      href="https://mega.nz/folder/0Sg0iD4B#0OPF1JJgFjtYoJuStlsCtA"
+                      href="https://mega.nz/folder/Ibl2CSIY#em77k1KkiqhFsQKXZkHdGw"
                       target="_blank"
                     >
                       מהמגה של האוניברסיטה הפתוחה
                     </Link>
                   </li>
                 </div>
-                <div className="flex gap-4 items-center mb-3">
+                <div className="mb-3 flex items-center gap-4">
                   <div>
                     <FaCheck />
                   </div>
                   <li> סידור השאלות לפי פרקים</li>
                 </div>
-                <div className="flex gap-4 items-center">
+                <div className="flex items-center gap-4">
                   <div>
                     <FaCheck />
                   </div>
@@ -115,7 +115,7 @@ const Home = () => {
               </ul>
               <div className="flex justify-center">
                 <Link
-                  className="btn inline-block btn-primary mt-5"
+                  className="btn btn-primary mt-5 inline-block"
                   href="/courses/algebra-1/bases/תלות-לינארית"
                 >
                   דוגמא
@@ -130,31 +130,22 @@ const Home = () => {
         <div
           ref={ref3}
           className={`opacity-0 ${
-            isVisible3 ? "animate-slideFromDown" : "opacity-0"
+            isVisible3 ? 'animate-slideFromDown' : 'opacity-0'
           }`}
         >
-          <div className="flex justify-center mb-10">
-            <div
-              className="
-            border
-            border-gray-200
-            dark:border-gray-700
-          rounded-full
-          px-3 py-1
-          text-sm
-        "
-            >
+          <div className="mb-10 flex justify-center">
+            <div className="rounded-full border border-gray-200 px-3 py-1 text-sm dark:border-gray-700">
               {"✨ פיצ'רים של עורך הטקסט"}
             </div>
           </div>
-          <div className="flex flex-col md:flex-row md:flex-wrap items-center gap-y-10 justify-center gap-x-5 ">
+          <div className="flex flex-col items-center justify-center gap-x-5 gap-y-10 md:flex-row md:flex-wrap">
             <Features
-              imgUrl={"/images/illustration-webgl.png"}
-              topText={"איור"}
+              imgUrl={'/images/illustration-webgl.png'}
+              topText={'איור'}
             >
               {ul2}
               <Link
-                className="text-blue-500 hover:underline font-arial font-extrabold"
+                className="font-arial font-extrabold text-blue-500 hover:underline"
                 href="https://excalidraw.com"
                 target="_blank"
               >
@@ -163,12 +154,12 @@ const Home = () => {
               {ul7}
             </Features>
             <Features
-              imgUrl={"/images/illustration-mathematics.png"}
-              topText={"מתמטיקה"}
+              imgUrl={'/images/illustration-mathematics.png'}
+              topText={'מתמטיקה'}
             >
               {ul2}
               <Link
-                className="text-blue-500 hover:underline font-arial font-extrabold"
+                className="font-arial font-extrabold text-blue-500 hover:underline"
                 href="https://cortexjs.io/mathlive"
                 target="_blank"
               >
@@ -177,12 +168,12 @@ const Home = () => {
               {ul3}
             </Features>
             <Features
-              imgUrl={"/images/illustration-3d-software.png"}
-              topText={"גרפים"}
+              imgUrl={'/images/illustration-3d-software.png'}
+              topText={'גרפים'}
             >
               {ul2}
               <Link
-                className="text-blue-500 hover:underline font-arial font-extrabold"
+                className="font-arial font-extrabold text-blue-500 hover:underline"
                 href="https://www.geogebra.org"
                 target="_blank"
               >
@@ -191,33 +182,24 @@ const Home = () => {
               {ul5}
             </Features>
           </div>
-          <div className="flex justify-center my-10">
-            <div
-              className="
-            border
-            border-gray-200
-            dark:border-gray-700
-          rounded-full
-          px-3 py-1
-          text-sm
-        "
-            >
-              {"נסו בעצמכם"}
+          <div className="my-10 flex justify-center">
+            <div className="rounded-full border border-gray-200 px-3 py-1 text-sm dark:border-gray-700">
+              {'נסו בעצמכם'}
             </div>
           </div>
-          <div className="flex justify-center mb-10">
-            <div className="font-arial min-h-[500px] w-2/3 max-w-2xl rounded-md border border-gray-400 dark:border-gray-400 flex flex-col relative">
+          <div className="mb-10 flex justify-center">
+            <div className="relative flex min-h-[500px] w-2/3 max-w-2xl flex-col rounded-md border border-gray-400 font-arial dark:border-gray-400">
               <Editor document={document} />
             </div>
           </div>
-          <div className="flex justify-center gap-6 mt-6">
+          <div className="mt-6 flex justify-center gap-6">
             <Link
               className="btn-outline-primary inline-block pt-2"
               href="/playground"
             >
               סביבת ניסוי
             </Link>
-            <Link className="btn inline-block btn-primary" href="/tutorial">
+            <Link className="btn btn-primary inline-block" href="/tutorial">
               מדריך
             </Link>
           </div>
